2894eb8f8e775ea707a2d154f96f6ef7f0af3c10
dg25moravian <99268114+dg25moravian@users.noreply.github.com>
2024-10-27 20:14:58 -0400

    Update userdata.sh

--- src/userdata.sh
+++ src/userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-yum install -y git
+yum install -y git python3
 git clone https://github.com/cs399f24/rocketchat-ec2
-cd rocketchat-ec2
-cd src
+cd rocketchat-ec2/src
 python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt
-gunicorn -w 1 -b 0.0.0.0:80 app:app
+source .venv/bin/activate
+pip install -r requirements.txt
+gunicorn -w 1 -b 0.0.0.0:80 app:app --daemon
