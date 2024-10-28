05362b0c1319a6926b390cb21aa8498187f038a6
fpera0248 <fperalta0248@gmail.com>
2024-10-28 18:16:10 -0400

    3

--- src/userdata.sh
+++ src/userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install -y git python3
-git clone https://github.com/cs399f24/rocketchat-ec2
-cd rocketchat-ec2/src
+git clone https://github.com/fpera0248/399_CloudComputingFinal.git
+cd 399_CloudComputing/src
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
