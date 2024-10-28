6ed3eb949922031779cee2f9d40300a93569f023
fpera0248 <fperalta0248@gmail.com>
2024-10-28 17:28:45 -0400

    2

--- src/userdata.sh
+++ src/userdata.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
-
-# Update system and install necessary packages
-yum update -y
 yum install -y git python3
-
-# Clone the specified repository
-cd /home/ec2-user
-git clone https://github.com/fpera0248/399_CloudComputingFinal
-cd 399_CloudComputingFinal/src
-
-# Set up Python virtual environment
+git clone https://github.com/cs399f24/rocketchat-ec2
+cd rocketchat-ec2/src
 python3 -m venv .venv
 source .venv/bin/activate
-
-# Install required packages from requirements.txt
 pip install -r requirements.txt
-
-# Copy the service file to the systemd directory and set correct permissions
-cp chatroom.service /etc/systemd/system/chatroom.service
-chmod 644 /etc/systemd/system/chatroom.service
-
-# Reload systemd daemon to recognize new service, enable it, and start the service
-systemctl daemon-reload
+cp chatroom.service /etc/systemd/system
 systemctl enable chatroom
 systemctl start chatroom
-
-# Check and log the status of the chatroom service
-systemctl status chatroom > /home/ec2-user/chatroom_setup.log 2>&1
