21d932e4fea79bf2850578d41abf12dec0e85b8b
fpera0248 <fperalta0248@gmail.com>
2024-10-28 18:21:29 -0400

    4

--- src/userdata.sh
+++ src/userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install -y git python3
 git clone https://github.com/fpera0248/399_CloudComputingFinal.git
-cd 399_CloudComputing/src
+cd 399_CloudComputingFinal/src
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
